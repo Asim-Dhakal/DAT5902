0eeeb58c72af3799af3ccf082b1608da777838ea
Asim <64024831+Asim-Dhakal@users.noreply.github.com>
2024-10-18 15:14:02 +0100

    Add files via upload

---
+++ Test_suite.py
@@ -0,0 +1,22 @@
+import unittest
+from Week2 import NumberIsEven
+
+class TestFunction(unittest.TestCase):
+
+    #tests with Odd number
+    def test_Odd_number(self):
+        result = NumberIsEven(5)
+        self.assertEqual(result, False)
+
+    # tests with Even number 
+    def test_Even_number(self):
+        result = NumberIsEven(4)
+        self.assertEqual(result, True)
+
+    # tests with Odd number 
+    def test_None_number(self):
+        result = NumberIsEven('String')
+        self.assertEqual(result, False)
+        
+if __name__ == '__main__':
+    unittest.main()
